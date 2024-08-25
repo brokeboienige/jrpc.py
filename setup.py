1e63ffc854dd6a79140aebf29ab1909df5f88175
brokeboienige <enige@protonmail.com>
2024-08-25 13:23:59 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ install_requires = {
 }
 setup(name='jrpc.py',
       version='0.1',
-      url='https://github.com/Enige1337/jrpc.py',
+      url='https://github.com/brokeboienige/jrpc.py',
       license='MIT',
-      author='Enige1337',
+      author='brokeboienige',
       author_email='enige@protonmail.com',
       description='A easy to use python library for Xx jAmes t xX\'s Xbox 360 JRPC plugin.',
       packages=['jrpc'],
